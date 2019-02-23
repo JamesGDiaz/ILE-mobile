9c62c15495a911bec918790a1c1d48f12b62f12d
Franco Giordani <jgdames@gmail.com>
2019-02-23 00:43:02 -0600

    remove commitgit.sh

--- commitgit.sh
+++
@@ -1,6 +0,0 @@
-echo "# ILE-mobile" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/JamesGDiaz/ILE-mobile.git
-git push -u origin master
